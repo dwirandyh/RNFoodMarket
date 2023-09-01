f0bb7c1c48d8088f4ba51bbba2dc98cf733c339c
Dwi Randy H <dwirandyherdinanto@gmail.com>
2023-09-01 16:48:51 +0700

    fix(profile): fix type on profile menu

--- src/components/molecules/ProfileTabSection/index.tsx
+++ src/components/molecules/ProfileTabSection/index.tsx
@@ -24,7 +24,7 @@ const Account = () => {
             <ItemListMenu text='Home Address' />
             <ItemListMenu text='Security' />
             <ItemListMenu text='Payments' />
-            <ItemListMenu text='SignOut' onPress={signOut} />
+            <ItemListMenu text='Sign Out' onPress={signOut} />
         </View>
     )
 }
